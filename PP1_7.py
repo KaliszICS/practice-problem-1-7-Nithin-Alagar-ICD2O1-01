3aaf3f199585f002c97cca24a2745eb0a37da3a2
Nithin-Alagar-ICD2O1-01 <nithin.alagar@student.tdsb.on.ca>
2024-09-26 13:16:07 +0000

    nithin

--- PP1_7.py
+++ PP1_7.py
@@ -1,25 +1,31 @@
-
-
-q1():
+def q1():
   #Write code here
-
-q2():
+ booll = True
+ print(booll)
+def q2():
+#Write code here
+  booll = int(print("Input an integer: "))
+  print(booll<5)
+def q3():
   #Write code here
-
-q3():
+  user1 = input("Input the letter a:")
+  print(user1 = a)
+def q4():
   #Write code here
-
-q4():
+  user2 = input("Input a word earlier in the dictionary than google: ")
+  print(user2>google)
+def q5():
   #Write code here
-
-q5():
-  #Write code here
-
+  user3 = input("Input an integer: ")
+  user4 = input("Input another integer: ")
+  result = user3*user4 > 40
+  print("Your numbers multiplied together are greater than 40: ", result)
 #Do edit the code below
 #Comment the lines below when running your tests
-
+'''
 q1()
 q2()
 q3()
 q4()
 q5()
+'''
