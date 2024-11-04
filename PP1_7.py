3ff9425964219325db2b4126a2ce1b19ddb895e7
Nithin-Alagar-ICD2O1-01 <nithin.alagar@student.tdsb.on.ca>
2024-11-04 21:30:12 +0000

    nithin

--- PP1_7.py
+++ PP1_7.py
@@ -4,22 +4,25 @@ def q1():
  print(booll)
 def q2():
 #Write code here
-  booll = int(input("Input an integer: "))
-  print(booll>=5)
+  booll = input("Input an integer: ")
+  booll = float(booll2)
+  booll2 = booll > 5
+  print(booll2)
 def q3():
   #Write code here
   user1 = input("Input the letter a:")
-  print(user1 == "a")
+  user15 = user1 == "a"
+  print(user15)
 def q4():
   #Write code here
   user2 = input("Input a word earlier in the dictionary than google: ")
   print(user2<"google")
 def q5():
   #Write code here
-  user3 = int(input("Input an integer: "))
-  user4 = int(input("Input another integer: "))
-  result = user3*user4 > 40
-  print("Your numbers multiplied together are greater than 40: ",result)
+  user3 = input("Input an integer: ")
+  user4 = input("Input another integer: ")
+  result = int(user3)*int(user4) > 40
+  print(f"Your numbers multiplied together are greater than 40: {result}")
 #Do edit the code below
 #Comment the lines below when running your tests
 
