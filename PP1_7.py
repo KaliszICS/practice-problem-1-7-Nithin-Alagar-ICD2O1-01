b388f7b397f589945b32745a63ee49f05577befb
Nithin-Alagar-ICD2O1-01 <nithin.alagar@student.tdsb.on.ca>
2024-11-04 11:27:45 -0500

    Update PP1_7.py

--- PP1_7.py
+++ PP1_7.py
@@ -9,7 +9,7 @@ def q2():
 def q3():
   #Write code here
   user1 = input("Input the letter a:")
-  print(user1 != "a")
+  print(user1 == "a")
 def q4():
   #Write code here
   user2 = input("Input a word earlier in the dictionary than google: ")
@@ -19,7 +19,7 @@ def q5():
   user3 = int(input("Input an integer: "))
   user4 = int(input("Input another integer: "))
   result = user3*user4 > 40
-  print("Your numbers multiplied together are greater than 40: ", result)
+  print("Your numbers multiplied together are greater than 40: ",result)
 #Do edit the code below
 #Comment the lines below when running your tests
 '''
